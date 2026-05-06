11b299d08df08bbc86ffafa5d0fc3ece55888320
zzwwaann <zzwwaann2017@gmail.com>
2026-05-07 01:25:25 +0300

    TypesOfData_Task2

---
+++ TypesOfData_Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿static void DrawRhombus(int N)
+{
+    int center = N / 2;
+    for (int i = 0; i <= center; i++)
+    {
+        Console.Write(new string(' ', center - i));
+        if (i == 0)
+        {
+            Console.Write("X");
+        }
+        else
+        {
+            Console.Write("X");
+            Console.Write(new string(' ', i * 2 - 1));
+            Console.Write("X");
+        }
+        Console.WriteLine();
+    }
+    for (int i = center - 1; i >= 0; i--)
+    {
+        Console.Write(new string(' ', center - i));
+        if (i == 0)
+        {
+            Console.Write("X");
+        }
+        else
+        {
+            Console.Write("X");
+            Console.Write(new string(' ', i * 2 - 1));
+            Console.Write("X");
+        }
+        Console.WriteLine();
+    }
+}
+
+DrawRhombus(5);
