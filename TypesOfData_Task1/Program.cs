5647dd66470ca28e5b2577a6427802b18ac0f3f6
zzwwaann <zzwwaann2017@gmail.com>
2026-05-06 14:05:45 +0300

    Добавьте файлы проекта.

---
+++ TypesOfData_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿double initialDeposit = 1000;
+int years = 3;
+double interestRate = 10;
+
+string result = CalculateCompoundInterest(initialDeposit, years, interestRate);
+Console.WriteLine(result);
+
+static string CalculateCompoundInterest(double initialDeposit, int years, double interestRate)
+{
+    string result = "";
+    double currentAmount = initialDeposit;
+
+    for (int i = 0; i < years; i++)
+    {
+        currentAmount = currentAmount * (1 + interestRate / 100);
+        result = result + $"Год {i + 1}: {currentAmount:F2} руб.\n";
+    }
+
+    return result;
+}
